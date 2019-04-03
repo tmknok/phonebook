0d1573ebd1615f2746bc4331395a496336daf4d3
tmknok <tiinan.m@gmail.com>
2019-04-03 20:33:54 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Matti Tienari",
+        number: "040-123456",
+        id: 2
+    },
+    {
+        name: "Arto Järvinen",
+        number: "040-123456",
+        id: 3
+    },
+    {
+        name: "Lea Kutvonen",
+        number: "040-123456",
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('Phonebook')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * Math.floor(100000))
+    return id
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    const name = persons.find(person => person.name === body.name)
+
+    if (body.name === undefined) {
+        return res.status(400).json({ error: 'name missing' })
+    }
+    if (body.number === undefined) {
+        return res.status(400).json({ error: 'number missing' })
+    }
+    if (name) {
+        return res.status(400).json({ error: `name ${body.name} already exists` })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
